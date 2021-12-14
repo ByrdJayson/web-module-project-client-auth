597040906c795ff44040a53db010c6a4e799e674
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-12-14 16:03:58 -0500

    Initial Framing Started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-
+import Login from './components/Login.js'
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <h1>Friends List Test App - Client Auth</h1>
+      <Login />
     </div>
   );
 }
