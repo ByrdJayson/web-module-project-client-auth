a5c01fa6b6364cf12f0a98acf436e7a61bbcde8a
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-12-14 22:16:06 -0500

    Forgot componentDidMount() only works in class components - Swapped for useEffect()

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import axiosWithAuth from '../utils/axiosWithAuth';
 
 function FriendsList() {
+
+    const [ friends, setFriends ] = useState([]);
+
+    useEffect(() => {
+        axiosWithAuth().get('/friends')
+        .then(res => {
+            console.log(res);
+        })
+        
+    }, [])
+
     return (
-        <div>
-            <h1>Friends List Test</h1>        
-        </div>
+        <>
+            <div>
+                <h1>Friends List Test</h1>        
+            </div>
+            <div className='friends'>
+
+            </div>
+        </>
+        
     )
 }
 
