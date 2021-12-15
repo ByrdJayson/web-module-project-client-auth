8a4e6c9d6d8e5cc108827d4ed9814affb5584274
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-12-14 21:15:27 -0500

    Component Framing Start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter, BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './components/Login.js'
+import FriendsList from './components/FriendsList';
 function App() {
   return (
     <div className="App">
       <h1>Friends List Test App - Client Auth</h1>
-      <Login />
+      <Login path='/'/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/friendsList' component={FriendsList}/> 
+          
+        </Routes>  
+
+      </BrowserRouter>
+      
+      
     </div>
   );
 }
---
+++ src/components/FriendsList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FriendsList() {
+    return (
+        <div>
+            <h1>Friends List Test</h1>        
+        </div>
+    )
+}
+
+export default FriendsList
