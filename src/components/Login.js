fec30cb7bd859f98595d1c5b7a75a6af84fd9069
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-12-14 21:38:37 -0500

    Login Form Created w/Change Handler

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const creds = {
+    username: '',
+    password: ''
+}
+
+const initialForm = {
+    username: '',
+    password: ''
+}
 
 function Login() {
+
+    const [formValues, setFormValues] = useState(initialForm);
+
+    const onChange = (e) => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+            
+        });
+        console.log(e);
+        console.log(e.target);
+        console.log(e.target.value);
+    }
+
     return (
+        <>
         <div>
-            <h1>Login Test</h1>
+            <h1>Login</h1>
         </div>
+        <div className='login-form form'>
+            <form>
+                <div>
+                    <label htmlFor='username'>Username:</label>
+                    <input name='username' type='text' value={formValues.username} onChange={onChange}/>
+                </div>
+                <div>
+                    <label htmlFor='password'>Password:</label>
+                    <input name='password' type='text' value={formValues.password} onChange={onChange}/>   
+                </div>
+                <button>Login</button>
+            </form>
+        </div>
+        
+        </>
+        
     )
 }
 
